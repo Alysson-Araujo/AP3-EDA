fe6164e7b91f0e8526a6fff6a7fa8b177abd8484
junivion <juninhocosta@yahoo.com.br>
2020-10-16 23:45:40 -0300

    problema 1

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void bfs(Graph *G, int start) {
 
     for (int i = 0; i < G->getN(); i++) {
         G->setMark(i, 1);         //todo mundo é branco. Logo, não foram visitados
-        G->setDistancia(i, INFI); //maior inteiro que cabe em um int
+        G->setDistancia(i, 0); //maior inteiro que cabe em um int
         G->setPai(i, -1);
     }
     G->setDistancia(start, 0);
@@ -24,8 +24,8 @@ void bfs(Graph *G, int start) {
     while (!Q.empty()) {
         int v = Q.front();
         Q.pop();
-        //cout << "Vertice " << v << " visitado" << endl;
-        //cout << "cor = " << G->getCor(v) << " pai = " << G->getPai(v) << " distancia = " << G->getDistancia(v) << endl;
+       // cout << "Vertice " << v << " visitado" << endl;
+       // cout << "cor = " << G->getCor(v) << " pai = " << G->getPai(v) << " distancia = " << G->getDistancia(v) << endl;
         for (int &w : G->neighbors(v)) {
             if (G->getMark(w) == 1) {
                 G->setMark(w, 2);
@@ -52,14 +52,14 @@ void bfs(Graph *G, int start) {
 int main() {
 
     fstream arq;
-    arq.open("testeGrafos.txt", ios::in);
+    arq.open("grafos.txt", ios::in);
 
     Graph *grafo;
     //grafo = new GraphM(N);
     //vértice 1 e 2
     int v1, v2;
 
-    string linha, linha2;
+    string linha;
     int tamanho;
 
     /* pega o tamanho do grafo*/
@@ -69,86 +69,97 @@ int main() {
     //cout << "tamanho é : " << tamanho << endl;
     grafo = new GraphM(tamanho);
     bool falha = false;
-
+    //cout << "TAMANHO:" << tamanho << endl;
     while (tamanho > 0) {
-
+        
         while (linha[0] != '0' || linha[2] != '0') {
-
+            getline(arq,linha);
             std::stringstream s_stream(linha);
 
             string substr;
-            getline(arq, linha);
+            getline(s_stream, substr, ' ');
 
-            v1 = std::stoi(linha);
+            v1 = std::stoi(substr);
 
-            
-            int espaco;
-            int aux = 0;
-            for (int i = 0; linha.length() > i; i++) {
-                if (linha[i] == ' ')
-                    espaco = i;
-            }
+            getline(s_stream, substr, ' ');
+            v2 = std::stoi(substr);
 
-            linha2[0] = linha[espaco];
-            linha2[1] = linha[espaco+1];
-
-
-
-            v2 = std::stoi(linha2);
-    
             if (v1 == 0 && v2 == 0)
                 break;
-            
 
-
-            //cout << linha << " ";
-
-            //cout << linha2 << endl;
+            //cout << "v1: " << v1 << endl;
+            //cout << "v2: " << v2 << endl;
 
             grafo->addEdge(v1, v2);
             grafo->addEdge(v2, v1);
         }
 
         bfs(grafo, 0);
+    
+        bool teste = true;  //testa se tem irmãos de cor diferente
+       
 
-        /*
-        int tem_vermelho[tamanho], tem_preto[tamanho]; // vetor de inteiros para guardar a quantidade de vermelhos e pretos em cada camada
+        int tem_vermelho[tamanho-1], tem_preto[tamanho-1]; // vetor de inteiros para guardar a quantidade de vermelhos e pretos em cada camada
         
 
-        for (int i = 0; i < tamanho; i++) {
-            tem_vermelho[i] = 0;
+        for (int i = 0; i < tamanho-1; i++) {
+            tem_vermelho[i] = 0;         //zera os vetores
             tem_preto[i] = 0;
         }
 
-        for (int i = 0; i < tamanho; i++) {
-            if (grafo->getCor(i) == 1) { // se for vermelho
+        for (int i = 0; i < tamanho-1; i++) {
+            if(grafo->getCor(i) == 1){
                 tem_vermelho[grafo->getDistancia(i)]++;
-            } else{ // se for vermelho
+            }
+            else{
                 tem_preto[grafo->getDistancia(i)]++;
             }
         }
+        /*
+        cout << "tem vermelho ";
+        for (int i = 0; i < tamanho-1; i++){
+            cout <<  tem_vermelho[i];
+        }
+        cout << endl;
+        cout << "tem preto ";
+        for (int i = 0; i < tamanho-1; i++){
+            cout << tem_preto[i];
+        }
+        cout << endl;
+        */
 
-        for(int i = 0; i < tamanho; i++) {
+        for(int i = 0; i < tamanho-1; i++) {
             if(tem_vermelho[i] > 0 && tem_preto[i] > 0){
                 teste = false;
                 break;
             }
 
         }
-
+    
         
-*/
-        bool teste = true;
+
+        bool teste2 = true;  //testa se tem ligação entre irmãos
 
         for(int i = 0; i < tamanho; i++){  //laço testando testando se tem ligação entre vertices da mesma camada 
             for(int j = 0; j < tamanho; j++){
                 if( i != j && grafo->getDistancia(i) == grafo->getDistancia(j) && grafo->isEdge(i,j)){
-                    teste = false;
+                    teste2 = false;
                     break;
                 }
             }
         }
-        if(teste == false)
+
+        bool teste3 = true; // se existe um pai que é cor igual ao filho
+
+        for(int i = 0; i < tamanho; i++){
+            if(grafo->getPai(i) != -1)
+                if(grafo->getCor(i) == grafo->getCor(grafo->getPai(i))){
+                    teste3 = false;
+                    break;
+                }
+        }
+
+        if(teste == false || teste2 == false || teste3 == false)
             cout << "NAO\n";
         else{
             cout << "SIM\n";
@@ -162,7 +173,7 @@ int main() {
         if (tamanho > 0) {
             grafo->~Graph();
             grafo = new GraphM(tamanho);
-        }
+        }   
     }
 
     //grafo->correctColoring();
